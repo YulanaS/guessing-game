23b10e32b11161567c8be237e0145f77e7feb9ed
yulanas <yulana2006@mail.ru>
2022-08-16 02:20:52 +0300

    feat: add guessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minimum = min;
+        this.maximum = max;
     }
 
     guess() {
-
+        this.result = Math.round((this.minimum + this.maximum) / 2);
+        return this.result;
     }
 
     lower() {
-
+        this.maximum = this.result;
     }
 
     greater() {
-
+        this.minimum = this.result;
     }
 }
 
